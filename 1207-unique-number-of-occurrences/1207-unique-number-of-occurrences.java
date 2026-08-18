f456fcb8d6caac9edf37c530a2f3479a3891c854
Raj Gajare <rajgajare00@gmail.com>
2026-08-18 23:29:16 +0530

    Time: 4 ms (9.35%), Space: 43.8 MB (18.42%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,16 +1,25 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> freq= new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
-        }
-        Set<Integer> seen =new HashSet<>();
+        // Map<Integer,Integer> freq= new HashMap<>();
+        // for(int i=0;i<arr.length;i++){
+        //     freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
+        // }
+        // Set<Integer> seen =new HashSet<>();
 
-        for(int count:freq.values()){
-            if(!seen.add(count)){
-                return false;
-            }
+        // for(int count:freq.values()){
+        //     if(!seen.add(count)){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        TreeMap<Integer, Integer> freq = new TreeMap<>();
+        for (int num : arr) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
-        return true;
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int count : freq.values()) {
+            set.add(count);
+        }
+        return set.size() == freq.size();
     }
 }
