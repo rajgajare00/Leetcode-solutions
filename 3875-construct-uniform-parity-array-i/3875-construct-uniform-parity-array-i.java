3fdc057f4ff6a49ba84229cd05e81c9cbaa77feb
Raj Gajare <rajgajare00@gmail.com>
2026-09-02 10:11:04 +0530

    Time: 0 ms (100%), Space: 44.4 MB (99.61%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        boolean sameparity=true;
+        int firstparity=nums1[0]%2;
+        for(int num:nums1){
+            if(num%2!=firstparity) {
+                sameparity=false;
+                break;
+            }
+
+        }
+        if (sameparity) return true;
+        return true;
+
+    }
+}
