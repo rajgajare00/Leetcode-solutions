2f82fe8e83fa9f185bba6d762023c1f7f488a73d
Raj Gajare <rajgajare00@gmail.com>
2026-09-09 01:14:59 +0530

    Time: 1 ms (83.58%), Space: 42.4 MB (85.34%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,19 +1,23 @@
 class Solution {
     public int countCommas(int n) {
-        int count = 0;
-        if (n >= 1000) {
-            count += Math.min(n, 999_999) - 999;
-        }
-        if (n >= 1_000_000) {
-            count += Math.min(n, 999_999_999) - 999_999;
-        }
-        if (n >= 1_000_000_000) {
-            count += Math.min(n, 999_999_999_999L) - 999_999_999;
-        }
-        if (n >= 1_000_000_000_000L) {
-            count += Math.min(n, 999_999_999_999_999L) - 999_999_999_999L;
-        }
+        // int count = 0;
+        // if (n >= 1000) {
+        //     count += Math.min(n, 999_999) - 999;
+        // }
+        // if (n >= 1_000_000) {
+        //     count += Math.min(n, 999_999_999) - 999_999;
+        // }
+        // if (n >= 1_000_000_000) {
+        //     count += Math.min(n, 999_999_999_999L) - 999_999_999;
+        // }
+        // if (n >= 1_000_000_000_000L) {
+        //     count += Math.min(n, 999_999_999_999_999L) - 999_999_999_999L;
+        // }
 
-        return count;
+        // return count;
+        if(n<1000){
+            return 0;
+        }
+        return n-999;
     }
 }
