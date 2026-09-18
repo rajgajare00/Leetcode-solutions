9b9e4333fde3bdf079dfea34c91214790730e761
Raj Gajare <rajgajare00@gmail.com>
2026-09-18 18:51:36 +0530

    Time: 1 ms (97.54%), Space: 43.6 MB (17.96%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,14 +1,23 @@
+// class Solution {
+//     public int scoreOfString(String s) {
+//         int[] result = new int[s.length()];
+
+//         for (int i = 0; i < s.length(); i++) {
+//             result[i] = (int) s.charAt(i);
+//         }
+//         int sum=0;
+//         int n=s.length();
+//         for(int i=1;i<n;i++ ){
+//             sum+=Math.abs(result[i]-result[i-1]);
+//         }
+//         return sum;
+//     }
+// }
 class Solution {
     public int scoreOfString(String s) {
-        int[] result = new int[s.length()];
-
-        for (int i = 0; i < s.length(); i++) {
-            result[i] = (int) s.charAt(i);
-        }
-        int sum=0;
-        int n=s.length();
-        for(int i=1;i<n;i++ ){
-            sum+=Math.abs(result[i]-result[i-1]);
+        int sum = 0;
+        for (int i = 1; i < s.length(); i++) {
+            sum += Math.abs(s.charAt(i) - s.charAt(i - 1));
         }
         return sum;
     }
